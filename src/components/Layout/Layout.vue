0055a41ab08a70be7be7bf3d611401ea27fb0820
raredead <smahelzed@gmail.com>
2023-12-03 13:20:44 +0300

    Правки страниц

--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -1,10 +1,22 @@
+<script setup lang="ts">
+import { useTheme } from 'vuetify';
+import { THEME_KEY } from '@/utils/constants'
+const theme = useTheme()
+
+const toggleTheme = () => {
+    const updateTheme = theme.global.current.value.dark ? 'light' : 'dark'
+    localStorage.setItem(THEME_KEY, updateTheme)
+    theme.global.name.value = updateTheme
+}
+</script>
+
 <template>
     <v-layout class="rounded rounded-md">
-        <v-app-bar title="Weather app"></v-app-bar>
+        <v-app-bar class="spacing-playground px-6" title="Weather app">
+            <v-icon @click="toggleTheme" icon="mdi-weather-sunny" />
+        </v-app-bar>
 
-        <v-main 
-        class="d-flex align-center justify-center" 
-        style="min-height: 300px;">
+        <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
             <slot />
         </v-main>
     </v-layout>
--- src/pages/Home/HomeApp.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <v-btn>Home</v-btn>
-</template>
